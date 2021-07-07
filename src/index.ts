d40fabd88720a1182ebab79429900b56f2b73dcf
Skylar Cupit <skylarcupit@gmail.com>
2021-07-06 23:58:41 -0500

    Copied type safe API code from my other project

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,52 @@
-import fs from "fs";
+import { BasicResponse } from "./ComplexApiResponse.ts";
+import { ComplexApiResponse } from "./ComplexApiResponse.ts";
+import { HttpStatus } from "./HttpStatus.ts";
 
-const tempNum = 3
-fs.writeFileSync("./Noice.txt", "MOOOOO")
-console.log(tempNum)
+interface Thing {
+  theData: string;
+}
+
+const res = new BasicResponse(
+  HttpStatus.OK,
+  {
+    theData: "Wow, very nice"
+  }
+); 
+
+const notFoundRes = new BasicResponse(
+  HttpStatus.NO_CONTENT,
+  null
+)
+
+const noContentRes = new BasicResponse(
+  HttpStatus.NO_CONTENT,
+  undefined
+)
+
+const temp = new ComplexApiResponse<{
+  // Honestly this looks like something I could use to simulate enum pattern matching.
+  OK: [Thing, string];
+  NOT_FOUND: [null, Thing];
+}>(
+  // res,
+  notFoundRes,
+  {
+    OK(body) {
+      return body.theData;
+    },
+    NOT_FOUND() {
+      return {
+        theData: "So sad, nothing was found"
+      };
+    }
+  }
+)
+
+temp.matchStatus({
+  OK(stringFromThing) {
+    console.log(`This is the fancy string: ${stringFromThing}`)
+  },
+  NOT_FOUND(body) {
+    console.log(body);
+  }
+})
